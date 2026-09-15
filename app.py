fd4b980b0ba8a3a94c801ab0e05d494ca7e267b8
brodrigueza <xbrauliox1@gmail.com>
2026-09-15 11:17:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def obtener_nombre(ticker):
 # 2. CARGA DE DATOS
 # ==========================================
 # (Aquí asumo que ya tienes tu código que lee tu Excel, CSV o base de datos)
-# df_portafolio = pd.read_excel("mis_inversiones.xlsx") 
+df_portafolio = pd.read_excel("mis_inversiones.xlsx") 
 
 # ==========================================
 # 3. PROCESAMIENTO DE YFINANCE
